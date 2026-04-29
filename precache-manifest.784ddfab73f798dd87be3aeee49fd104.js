d35ed7d2a4b58f97ce3c421ee684f314d1b41b89
unnatii-eb <unnati@eventbrite.com>
2026-04-29 17:25:03 +0530

    Updates

---
+++ precache-manifest.784ddfab73f798dd87be3aeee49fd104.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0180e16f75c0e84a8ff8",
+    "url": "/word-rush/static/css/main.3309d1d4.chunk.css"
+  },
+  {
+    "revision": "0180e16f75c0e84a8ff8",
+    "url": "/word-rush/static/js/main.0180e16f.chunk.js"
+  },
+  {
+    "revision": "4d361691d88489c20eb6",
+    "url": "/word-rush/static/js/runtime~main.4d361691.js"
+  },
+  {
+    "revision": "6f22f56b554b6b57f23d",
+    "url": "/word-rush/static/js/2.6f22f56b.chunk.js"
+  },
+  {
+    "revision": "1c2e5f4b2f11c164c8b51d225b4bd91c",
+    "url": "/word-rush/index.html"
+  }
+];
